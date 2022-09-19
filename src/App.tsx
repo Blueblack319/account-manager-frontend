57fd9cf6efd4a8cedfc066be0d657ef88f8d0302
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-20 08:58:20 +0900

    Test to get api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:5000/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({
+        email: 'test1@test.com',
+        password: 'test11'
+      })
+    }).then(res => res.json()).then(data => console.log(data)).catch(e => {
+      console.log(e.message)
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
