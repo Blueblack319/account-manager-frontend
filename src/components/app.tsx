7e58c60929f268cb2ec4b65f09ffb4d8d60c2c1d
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-20 10:20:35 +0900

    Set up routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LoggedOutRouter } from '../routers/logged-out-router';
 
 function App() {
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/user/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: 'test1@test.com',
-        password: 'test11',
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((e) => {
-        console.log(e.message);
-      });
-  });
+  // useEffect(() => {
+  //   fetch(`${process.env.REACT_APP_API_URL}/user/login`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       email: 'test1@test.com',
+  //       password: 'test11',
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((e) => {
+  //       console.log(e.message);
+  //     });
+  // });
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <LoggedOutRouter />;
 }
 
 export default App;
