334c0521278cb51977d1d9dd9b2d3be1570296d9
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-20 10:28:10 +0900

    Scaffold page structure

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export const LoggedOutRouter = () => {
-  return <span>Logged out router</span>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
