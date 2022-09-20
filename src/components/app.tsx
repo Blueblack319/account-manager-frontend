1e8dd26cdc0ba828f228485c740f149fba630f1c
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-20 10:03:09 +0900

    Add helmet

---
+++ src/components/app.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/user/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: 'test1@test.com',
+        password: 'test11',
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((e) => {
+        console.log(e.message);
+      });
+  });
+
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
-import App from './App';
+import App from './components/app';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
